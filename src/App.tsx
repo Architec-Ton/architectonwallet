175b3ad42446d55aefbe45616a748ef0ee9a18fd
Stan Takt <st.takt@gmail.com>
2024-04-08 08:57:54 +0300

    connection TON WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import twaLogo from './assets/tapps.png'
 import viteLogo from '/vite.svg'
+import { TonConnectUIProvider, TonConnectButton } from '@tonconnect/ui-react';
+import { ConnectedWalletInfo } from './assets/lib/ConnectedWalletInfo';
+
 import './App.css'
 
-import WebApp from '@twa-dev/sdk'
+// import WebApp from '@twa-dev/sdk'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const [count, setCount] = useState(0)
+  
   return (
+    
+      <TonConnectUIProvider manifestUrl="https://www.architecton.site/tonconnect-manifest.json"
+        actionsConfiguration={{
+        twaReturnUrl: 'https://t.me/architec_ton_bot/wallet/'
+    }}>
     <>
+    
       <div>
         <a href="https://ton.org/dev" target="_blank">
           <img src={twaLogo} className="logo" alt="TWA logo" />
@@ -22,20 +31,30 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>TWA + Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      {/*  */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-      </div>
+      <h1>ArchiTec Ton Wallet</h1>
+      <header>
+            <span>Connect to wallet</span>
+            <TonConnectButton />
+            <ConnectedWalletInfo />
+        </header>
+        <div>
+
+        
+          </div>
     </>
+    </TonConnectUIProvider>
   )
 }
 
 export default App
+/*  <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      {/*  }
+      <div className="card">
+        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+            Show Alert
+        </button>
+      </div> */
---
+++ src/assets/lib/ConnectedWalletInfo.ts
@@ -0,0 +1,12 @@
+import { useTonWallet } from '@tonconnect/ui-react';
+
+export const ConnectedWalletInfo = () => {
+    const wallet1 = useTonWallet();
+
+    return (
+            
+                wallet1
+            
+        
+    );
+};
